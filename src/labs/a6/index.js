043a1499164c7f94e0e055a6a499e1e96c516f16
K Vivekananda Reddy <vivekananda-reddy@users.noreply.github.com>
2023-02-22 23:48:05 -0500

    lab a6 completed

---
+++ src/labs/a6/index.js
@@ -0,0 +1,18 @@
+
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
+import Todos from "./todo/todo-list";
+
+function Assignment6() {
+    return (
+        <div>
+            <h1>Assignment 6</h1>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
+            <Todos/>
+        </div>
+    );
+}
+export default Assignment6;
---
+++ src/labs/index.js
@@ -0,0 +1,12 @@
+import Nav from "../nav";
+import Assignment6 from "./a6";
+
+function Labs() {
+    return(
+        <div>
+            <Nav/>
+            <Assignment6/>
+        </div>
+    );
+}
+export default Labs;
